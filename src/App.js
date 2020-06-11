331d184bbea49a23a8f4b14304b686b8a1e0cea2
rohanJa <rohan.jain150300@gmail.com>
2020-06-11 21:58:30 +0530

    UseEffect use

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
- 
+
+  // Similar to componentDidMount and componentDidUpdate:  
+  useEffect(() => {    // Update the document title using the browser API   
+     document.title = `You clicked ${count} times`;  });
   return (
-      <div>
-        <p>You clicked {count} times</p>
-        <button onClick={() => setCount(count + 1)}>
-          Increase
-        </button>
-        <button onClick={()=>setCount(count-1)}> Decrease</button>
-      </div>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
   );
 }
 
