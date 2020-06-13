f60e6cc4360953f2e6c2db750b25866b01f90ed5
rohanJa <rohan.jain150300@gmail.com>
2020-06-13 16:01:20 +0530

    Updates

---
+++ precache-manifest.3e23fa62ccd52e61c521af4caf860ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18949fc1853edd31d8f79612ed99c00d",
+    "url": "/learnReact/index.html"
+  },
+  {
+    "revision": "f9eccc3bad11e5d5f420",
+    "url": "/learnReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a3e32559c48dc19fd53",
+    "url": "/learnReact/static/js/2.689a8089.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learnReact/static/js/2.689a8089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9eccc3bad11e5d5f420",
+    "url": "/learnReact/static/js/main.ff65d8d1.chunk.js"
+  },
+  {
+    "revision": "eb3041fb04e133f85e91",
+    "url": "/learnReact/static/js/runtime-main.dd47ef5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learnReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
